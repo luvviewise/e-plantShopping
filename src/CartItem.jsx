cfb03277c395890c588ca54962e1c302255e6320
Luvvie Wise <vwise36@gmail.com>
2026-08-11 14:01:38 -0400

    Implement cartitems and slice  functionality

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { removeItem, updateQuantity } from './CartSlice';
+import { removeItem, updateQuantity } from './CartSlice.jsx';
 import './CartItem.css';
 
 const CartItem = ({ onContinueShopping }) => {
@@ -9,27 +9,42 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total= 0;
+    cart.forEach ((item) => {
+        const itemCost= parseFloat(item.cost.substring(1));
+        total+= itemCost * item.quantit;
+    });
+
+      return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
-
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
+  
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ id: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity >1) {
+        dispatch(updateQuantity({ id: item.name, quantity: item.quantity - 1 }));
+    } else {
+        dispatch(removeItem(item.name));
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return parseFloat(item.cost.substring(1)) * item.quantity;
   };
 
   return (
